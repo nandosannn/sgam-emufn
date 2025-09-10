62e10470b171c851e23b819d84ee39a785145523
Fernando Santos <158214975+nandosannn@users.noreply.github.com>
2025-09-10 09:57:46 -0300

    feat: concluido a modularização da página home

---
+++ resources/views/dashboard/content-header.blade.php
@@ -0,0 +1,36 @@
+<!--begin::App Content Header-->
+<div class="app-content-header">
+    <!--begin::Container-->
+    <div class="container-fluid">
+        <!--begin::Row-->
+        <div class="row">
+            <div class="col-sm-6">
+                @hasSection ('page-title')
+                     <h3 class="mb-0">
+                        @yield('page-title')
+                    </h3>
+                @endif
+
+
+                @isset($menus)
+                        <ol class="breadcrumb ">
+                            @foreach ($menus as $menu)
+                            <li class="breadcrumb-item">
+                                <a href="{{$menu['url']}}"> {{$menu['label']}} </a>
+                            </li>
+                            @endforeach
+                        </ol>
+
+                @endisset
+
+
+            </div>
+            <div class="col-sm-6">
+                Actions
+            </div>
+        </div>
+        <!--end::Row-->
+    </div>
+    <!--end::Container-->
+</div>
+<!--end::App Content Header-->
