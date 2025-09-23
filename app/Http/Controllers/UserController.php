5ab12f1d5e94cebd1e934d6b0affff6621ffdb1f
Fernando Santos <158214975+nandosannn@users.noreply.github.com>
2025-09-23 18:20:49 -0300

    feat: paginação

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index(){
 
-        $users = User::all();
+        $users = User::paginate();
 
         return view('users.index', compact('users'));
     }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -50,5 +50,9 @@
         </tbody>
     </table>
 </div>
+<div class="p-3">
+    {{ $users->links() }}
+</div>
+
 
 @endsection
