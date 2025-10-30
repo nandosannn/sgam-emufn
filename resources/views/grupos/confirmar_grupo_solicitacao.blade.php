e17ed43a6b18ea3e5f2b96afe6ad93a5cde53493
Fernando Santos <158214975+nandosannn@users.noreply.github.com>
2025-10-30 12:11:53 -0300

    feat: concluído o formulário de confirmação dos músicos

--- resources/views/grupos/confirmar_grupo_solicitacao.blade.php
+++ resources/views/grupos/confirmar_grupo_solicitacao.blade.php
@@ -11,9 +11,10 @@
 @endsession
     <div class="font container-fluid p-5 shadow-sm" style="background-color: #fcfcfcff;">
         <div class="fs-4 fw-bold mb-4">Confirmar Grupo</div>
-        <form action="{{route('informacoesconfirmacao.grupos', $solicitacao)}}" method="POST">
+        <form action="{{route('informacoesconfirmacao.grupos')}}" method="POST">
             @csrf
             @include('grupos.parts.card_confirmacao_grupo', ['solicitacao' => $solicitacao])
+            @include('grupos.parts.card_dados_endereco')
             <div class="card-footer text-end">
                 <button type="submit" class="btn btn-primary text-end">Confirmar</button>
             </div>
